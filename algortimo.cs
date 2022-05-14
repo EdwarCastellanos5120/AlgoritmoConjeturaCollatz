cb35433d1a926aa0d3d46ae68dfa43801e5586f1
Edwar <79104426+EdwarCastellanos5120@users.noreply.github.com>
2022-05-13 22:57:23 -0600

    Terminado

---
+++ algortimo.cs
@@ -0,0 +1,34 @@
+﻿Func<long, bool> isEven = (n) => n % 2 == 0;
+Action<long> algoritmoCollatz = null;
+ algoritmoCollatz = (n) =>
+{
+    Console.WriteLine(n);
+    long resultado = 0;
+    if (isEven(n))
+    {
+        resultado = n / 2; //SE OPERA n SI ES PAR
+    }
+    else
+    {
+        resultado = (n * 3) + 1; //SE OPERA n SI ES IMPAR
+    }
+
+    /*
+     * REFACTORIZAR EL ALGORITMO
+     * if (n != 1)
+       collatz(isEven(n)? n/2 : (n*3)+1 );
+     */
+
+    if (resultado!=1)
+    algoritmoCollatz(resultado);
+    else
+    {
+        Console.WriteLine(resultado);
+    }
+};
+
+algoritmoCollatz(13);
+Console.WriteLine("\n");
+algoritmoCollatz(10520124451);
+Console.WriteLine("\n");
+algoritmoCollatz(918741918981981781);
